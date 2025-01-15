5e75ca19c31acc876002618ae9787ee65f8a3c84
Thomas Johnson <thomasjohnson@Thomass-MacBook-Air.local>
2025-01-15 16:43:02 -0500

    generated User/Account models

---
+++ lib/gist_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule GistWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use GistWeb, :controller` and
+  `use GistWeb, :live_view`.
+  """
+  use GistWeb, :html
+
+  embed_templates "layouts/*"
+end
